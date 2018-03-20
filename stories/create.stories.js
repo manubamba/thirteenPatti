c9821038cb1f88e1c957f8c639e58766ea419923
Vineet Dev <vindev@microsoft.com>
2018-03-20 12:44:22 +0530

    Added common join page

--- stories/create.stories.js
+++ stories/create.stories.js
@@ -6,7 +6,11 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import CreateSession from '../src/components/createSession/CreateSession';
+import CommonJoin from "../src/components/CommonJoin/CommonJoin";
 
 
 storiesOf('Create Session', module)
   .add('Landing page', () => <CreateSession onClickCreate={action('Create session')}/>)
+
+storiesOf('Join Session', module)
+  .add('Common Join Page', () => <CommonJoin joinSession={action('Join Session')} />)
