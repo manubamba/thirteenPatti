8ab4f1d59788041934f303486546cd219f03f27b
REDMOND\rakeshma <rakeshma@microsoft.com>
2018-03-21 14:55:19 +0530

    Adding sockets code

--- server/startServer.js
+++ server/startServer.js
@@ -1,3 +1,19 @@
+/**
+ How to use sockets in client:
+ export function initCommunication() {
+    console.log('initializing');
+    io = socket('http://localhost:3001/');
+    io.on('updatestate', state => {
+        stateMutator(state);
+    });
+}
+
+To send message:
+io.emit('vote', {sessionId: 12345, participantId: 5, estimate: 10});
+*/
+
+var disableLogging = false;
+
 function startServer() {
     let app = require('express')();
     let http = require('http').Server(app);
@@ -13,37 +29,57 @@ function startServer() {
 
     // CreateSession - (SessionTitle, UserName) => (ParticipantId, state)
     app.post('/createsession', (req, res) => {
-        console.log('Creating new session');
+        Log('Creating new session');
         res.send(CreateSession(req.body.sessionTitle, req.body.userName));
     });
 
     // JoinSession - (SessionId, UserName) => (ParticipantId, state)
     app.post('/joinsession', (req, res) => {
-        console.log("User joining session");
+        Log("User joining session");
         res.send(JoinSession(req.body.sessionId, req.body.userName));
     });
 
-    // resetVotes - (ParticipantId) => ()
-    // finalize - (ParticipantId, estimate) => ()
-    // kick - (ParticipantId, BadParticipantId) => ()
-
     // Initializing web sockets
     io.on('connection', (socket) => { 
-        console.log('User connected');
+        Log('User connected');
+
+        // Clean up
+        socket.on('disconnect', reason => Log('Disconnected: ' + reason));
 
         // Vote - (sessionId, participantId, estimate) => ()
         socket.on('vote', state => {
-            console.log('Vote received');
-            let sessionId = state.sessionId;
-            let updatedState = state;
-            updatedState.sessionId++;
-            io.emit('updatestate', updatedState);
+            Log('Before voting: ' + sessionStates[state.sessionId]);
+            Vote(state.sessionId, state.participantId, state.estimate);
+            Log('After voting: ' + sessionStates[state.sessionId]);
+
+            // let updatedState = state;
+            // updatedState.sessionId++;
+            // io.emit('updatestate', updatedState);
         });
 
-        // Clean up log
-        socket.on('disconnect', reason => console.log('Disconnected: ' + reason));
+        // finalize - (sessionId, participantId, estimate) => ()
+        socket.on('finalize', state => {
+            Log('Before Finalizing: ' + sessionStates[state.sessionId]);
+            Finalize(state.sessionId, state.participantId, state.estimate);
+            Log('After Finalizing: ' + sessionStates[state.sessionId]);
+
+        });
+
+        // resetvotes - (sessionId, participantId) => ()
+        socket.on('resetvotes', state => {
+            Log('Before Resetting votes: ' + sessionStates[state.sessionId]);
+            ResetVotes(state.sessionId, state.participantId);
+            Log('After Resetting votes: ' + sessionStates[state.sessionId]);
+        });
+
+        // kickuser - (sessionId, participantId, badParticipantId) => ()
+        socket.on('kickuser', state => {
+            Log('Before kicking: ' + sessionStates[state.sessionId]);
+            KickUser(state.sessionId, state.participantId, state.badParticipantId);
+            Log('After kicking: ' + sessionStates[state.sessionId]);
+        });
     });
-    http.listen(3001, () => console.log('listening on port 3001'));
+    http.listen(3001, () => Log('listening on port 3001'));
 }
 
 startServer();
@@ -72,7 +108,7 @@ function CreateSession(sessionTitle, userName) {
         ],
         currentStoryIndex: 0,
         votes: [
-            {name: userName, participantId: nextParticipantId++, vote: undefined, isAdmin: true}
+            {name: userName, participantId: nextParticipantId++, estimate: undefined, isAdmin: true}
         ]
     };
 
@@ -91,8 +127,101 @@ function JoinSession(sessionId, userName) {
     sessionStates[sessionId].votes.push({
         name: userName,
         participantId: participantId,
-        vote: undefined,
+        estimate: undefined,
         isAdmin: false,
     });
     return {state: sessionStates[sessionId], participantId: participantId};
+}
+
+/**
+ * Registers the vote of the participant
+ * @param {id of the session} sessionId 
+ * @param {id of the participant} participantId 
+ * @param {estimate} estimate 
+ */
+function Vote(sessionId, participantId, estimate) {
+    let votes = sessionStates[sessionId].votes;
+    for(var vote in votes) {
+        if (vote.participantId == participantId) {
+            vote.estimate = estimate;
+            return;
+        }
+    }
+}
+
+/**
+ * Finalizes the vote of the story
+ * @param {id of the session} sessionId 
+ * @param {id of the participant} participantId 
+ * @param {estimate} estimate 
+ */
+function Finalize(sessionId, participantId, estimate) {
+    let sessionState = sessionStates[sessionId];
+
+    // Ensure the admin is asking to finalize
+    if (!isAdmin(sessionId, participantId)) {
+        // If the participant is not admin, do nothing
+        return;
+    }
+
+    let story = sessionState.stories[sessionState.currentStoryIndex];
+    story.finalVote = estimate;
+    story.state = 'finalized';
+}
+
+/**
+ * Resets the votes of the current story
+ * @param {id of the session} sessionId 
+ * @param {id of the participant} participantId 
+ */
+function ResetVotes(sessionId, participantId) {
+    if (!isAdmin(sessionId, participantId)) {
+        // if participant is not admin, do nothing
+        return;
+    }
+
+    let sessionState = sessionStates[sessionId];
+    for (var vote in sessionState.votes) {
+        vote.estimate = undefined;
+    }
+}
+
+/**
+ * Removes the bad participant from the session
+ * @param {id of the session} sessionId 
+ * @param {id of the participant} participantId 
+ * @param {id of the participant who needs to be kicked out} badParticipantId 
+ */
+function KickUser(sessionId, participantId, badParticipantId) {
+    if (!isAdmin(sessionId, participantId)) {
+        // if participant is not admin, do nothing
+        return;
+    }
+
+    let votes = sessionStates[sessionId].votes;
+    var i = 0;
+    for(var vote in votes) {
+        if (vote.participantId == participantId) {
+            votes.splice(i, 1);
+        }
+        i++;
+    }
+}
+
+// Utils
+function Log(line) {
+    if (!disableLogging) {
+        console.log(line);
+    }
+}
+
+function isAdmin(sessionId, participantId) {
+    let sessionState = sessionStates[sessionId];
+    for(var vote in sessionState.votes) {
+        if (vote.participantId == participantId) {
+            return vote.isAdmin;
+        }
+    }
+
+    return false;
 }
